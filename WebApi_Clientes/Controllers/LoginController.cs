a22d4b62fdec6f0a5f038a58201bd8b46dd585fc
rocipepek <rocipepek@gmail.com>
2020-05-13 14:20:23 -0300

    Agregar archivos de proyecto.

---
+++ WebApi_Clientes/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Taller.BussinesLogic;
+using Taller.Domain;
+
+namespace WebAPIClientes
+{
+    public class LoginController : ApiController
+    {
+        // POST api/<controller>
+        public string Post([FromBody]Usuario usuario)
+        {
+            string result = "Usuario o clave incorrecta";
+
+            List<Usuario> lista = UsuariosManager.Get();
+
+            foreach(Usuario user in lista)
+            {
+                if (((usuario.User).Equals(user.User) && (usuario.Clave).Equals(user.Clave)))
+                {
+                    if (user.NLogins < 3)
+                    {
+                        UsuariosManager.Actualizar(usuario);
+                        result = "Usuario autenticado";
+                   }
+                    else
+                    {
+                        result = "Usuario bloqueado";
+                    }
+                   
+                    
+                }
+                else if ((usuario.User).Equals(user.User) && !((usuario.Clave).Equals(user.Clave)))
+                {
+                    if (user.NLogins < 3)
+                    {
+                        usuario.NLogins = user.NLogins + 1;
+                        UsuariosManager.Actualizar(usuario);
+                        result = "Usuario o clave incorrecta";
+                    }
+                    else
+                    {
+                        result = "Usuario bloqueado";
+                    }
+                    
+                }
+            }
+            return result;
+        }
+       
+ 
+
+    }
+}
---
+++ WebApi_Clientes/scripts/main.js
@@ -0,0 +1,45 @@
+﻿$(function () {
+    $('#btnLogin').click(function () { loginUsuario(); });
+});
+
+function ajax(strUsuario,strClave, strFecha) {
+    var result;
+    var usuario = {};
+    usuario.User = strUsuario;
+    usuario.Clave = strClave;
+    usuario.FechaLogin = strFecha;
+
+    $.ajax({
+        url: 'http://localhost:6030/api/login',
+        type: 'POST',
+        data: usuario,
+        async: false
+    }).done(function (data) {
+        result = data;
+    }).error(function (xhr, status, error) {
+        alert(error);
+        var s = status;
+        var e = error;
+    });
+
+    return result;
+}
+
+function loginUsuario() {
+    var usuario = $("#txtUsuario").val();
+    var clave = $("#txtClave").val();
+    
+    var d = new Date();
+
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
+
+    var output = d.getFullYear() + '/' +
+        (month < 10 ? '0' : '') + month + '/' +
+        (day < 10 ? '0' : '') + day;
+
+
+    var result = ajax(usuario, clave, output);
+    alert(result);
+}
+
